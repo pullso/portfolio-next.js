48a62eeb09939cf2db7fa7caed8a32f9cba0e8e9
pullso <pullso.code@gmail.com>
2023-04-29 22:43:54 +0300

    react-buddy has been removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,12 @@ import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
 import Head from "next/head";
 import React from "react";
-import {DevSupport} from "@react-buddy/ide-toolbox-next";
-import {ComponentPreviews, useInitial} from "@/components/dev";
 
 export default function App({Component, pageProps}: AppProps) {
   return <>
     <Head>
       <title>Pavel Dubitskii - Frontend developer</title>
     </Head>
-    <DevSupport ComponentPreviews={ComponentPreviews}
-                useInitialHook={useInitial}
-    >
       <Component {...pageProps} />
-    </DevSupport>
   </>
 }
